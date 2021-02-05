76e03ee9b41b6f50b5d5641052b965aa2ab646ca
sweetpotato27 <matthewsdb34@gmail.com>
2021-02-05 14:25:10 -0800

    html table grid and keyup event handler

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import _ from 'lodash';
 import './style.css';
 import Icon from './icon.png';
 
-const Grid = require("./grid");
-const GameLoop = require("./game_loop");
-
 function component() {
     const element = document.createElement('div');
 
@@ -27,34 +24,14 @@ function headComponent() {
 }
 
 document.head.appendChild(headComponent());
-document.body.appendChild(component());
-
-
 
 
 document.addEventListener("DOMContentLoaded", function () {
-    const canvas = document.getElementById("canvas");
-    const generate = document.getElementById("generate");
-    const reset = document.getElementById("reset");
-    
-    canvas.width = 500;
-    canvas.height = 500;
-    // canvas.style.zIndex = 1;
-
-    const ctx = canvas.getContext("2d");
-    const grid = new Grid();
-    const game = new GameLoop(grid, ctx);
-    grid.setup(ctx);
-    game.start();
-
-    generate.onclick = function gen() {
-        grid.generate = !grid.generate;
-    }
-
-    reset.onclick = function res() {
-        grid.reset();
-    }
-
+    document.addEventListener("keyup", (event) => {
+        let player = document.getElementsByClassName('player')[0];
+        console.log(player);
+        player.innerText = "";
+    });
 });
 
 
