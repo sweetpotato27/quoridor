822b998cdb82c0fc03d700506189c4ef5376093f
sweetpotato27 <matthewsdb34@gmail.com>
2021-02-05 16:11:44 -0800

    unvalidated movement complete

--- src/index.js
+++ src/index.js
@@ -28,11 +28,84 @@ document.head.appendChild(headComponent());
 
 document.addEventListener("DOMContentLoaded", function () {
     document.addEventListener("keyup", (event) => {
+        const columns = ["a","b","c","d","e","f","g","h","i"]
         let player = document.getElementsByClassName('player')[0];
-        console.log(player);
-        player.innerText = "";
+        let newPlayer;
+        let newDest = "";
+        if(event.key === "ArrowUp" || event.key === "ArrowDown"
+            || event.key === "ArrowRight" || event.key === "ArrowLeft") {
+            if(event.key === "ArrowUp") {
+                let dest = player.id;
+                //if valid move move up
+                if(true) {
+                    // turn into an array for easy change
+                    newDest = newDest.split("");
+                    dest = dest.split("");
+                    newDest[0] = dest[0];
+                    newDest[1] = parseInt(dest[1]) - 1;
+                    newDest = newDest.join("");
+                }
+                console.log(newDest);
+            } else if(event.key === "ArrowDown") {
+                let dest = player.id;
+                //if valid move move up
+                if(true) {
+                    // turn into an array for easy change
+                    newDest = newDest.split("");
+                    dest = dest.split("");
+                    newDest[0] = dest[0];
+                    newDest[1] = parseInt(dest[1]) + 1;
+                    newDest = newDest.join("");
+                }
+                console.log(newDest);
+            } else if(event.key === "ArrowRight") {
+                let dest = player.id;
+                //if valid move move up
+                if(true) {
+                    // turn into an array for easy change
+                    newDest = newDest.split("");
+                    dest = dest.split("");
+                    // finds the index of letter in 'columns' then indexes one more
+                    newDest[0] = columns[findColumnIndex(columns, dest[0]) + 1];
+                    newDest[1] = dest[1];
+                    newDest = newDest.join("");
+                }
+                console.log(newDest);
+            } else if (event.key === "ArrowLeft") {
+                let dest = player.id;
+                //if valid move move up
+                if(true) {
+                    // turn into an array for easy change
+                    newDest = newDest.split("");
+                    dest = dest.split("");
+                    // finds the index of letter in 'columns' then indexes one more
+                    newDest[0] = columns[findColumnIndex(columns, dest[0]) - 1];
+                    newDest[1] = dest[1];
+                    newDest = newDest.join("");
+                }
+                console.log(newDest);
+            }
+            newPlayer = document.getElementById(newDest);
+            player.innerHTML = "";
+            player.classList.remove("player");
+            newPlayer.innerHTML = "X";
+            newPlayer.classList.add("player");
+        }
     });
 });
+
+function findColumnIndex(arr, target) {
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] === target) {
+            return i;
+        }
+    }
+}
+
+function validMove(dest) {
+
+}
+
 
 
 
