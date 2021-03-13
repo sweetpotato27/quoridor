11d6d3514eeb1abde623c1e21bfcd890854a096f
Dylan Matthews <dmatthews@builder.local>
2021-03-12 22:37:03 -0800

    thirteenth

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ const leaveRooms = (socket) => {
 };
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static({publicPath: config.output.path}));
+    app.use(config.output.path);
 
     app.get('/', (req, res) => {
         res.sendFile(path.resolve(__dirname + 'index.html'));
